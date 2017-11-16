071da18ad2fb7f94fb78bcb3d13d77d1dfbf5a02
samstudy <samat.study@gmail.com>
2017-11-16 21:23:47 +0600

    Define useful inf in model

--- model.py
+++ model.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://score:Rysherat2@shopscore.devman.org:5432/shop'
 db = SQLAlchemy(app)
-db.Model.metadata.reflect(db.engine)
+
 
 class Orders(db.Model):
     __tablename__ = 'orders'
+    created = db.Column(db.DateTime, primary_key = True)
+    status = db.Column(db.Text)
+    confirmed = db.Column(db.DateTime)
